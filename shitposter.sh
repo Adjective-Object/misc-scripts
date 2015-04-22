366422216b70ed01a50a3989f1c76455f9e63df0
Adjective-Object <mhuan13@gmail.com>
2015-04-21 21:05:06 -0400

    removed shitposter printing and some unecessary code

--- shitposter.sh
+++ shitposter.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-apply_shell_expansion() {
-    declare file="$1"
-    declare data=$(< "$file")
-    declare delimiter="__apply_shell_expansion_delimiter__"
-    declare command="cat <<$delimiter"$'\n'"$data"$'\n'"$delimiter"
-    eval "$command"
-}
-
 boards=(a c w m cgl c f n jp vp v vg vr co g tv k o an tg sp asp sci int out\
 toy biz i po p ck ic wg mu fa 3 gd diy wsg trv fit x lit adv lgbt mlp b r r9k\
 pol soc s4s s hc hm h e u d y t hr gif)
@@ -24,12 +16,9 @@ wget -O board.html "http://boards.4chan.org/$board/"
 xmllint --html --xmlout board.html 2>/dev/null > board.xml
 
 payload=$(xqilla shitposter.xq)
-api_hook='https://hooks.slack.com/services/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'
 
 payload="${payload//\$board/$board}"
 
-echo "$payload"
-
-curl -X POST --data-urlencode "$payload" "$api_hook"
+curl -X POST --data-urlencode "$payload" "$SLACK_SHITPOST_HOOK"
 rm board.html
 rm board.xml
